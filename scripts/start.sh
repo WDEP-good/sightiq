5313baaaa9a2b4342e06beef08b7080345221c1f
WDEP <chenzc6@chanjet.com>
2025-05-08 13:53:08 +0800

    添加containerd代理配置

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,17 +4,31 @@ echo "开始启动项目..."
 
 export PROXY_IP=$1
 
-
-function setProxy() {   
+function setProxy() {
     echo "设置代理..."
     export http_proxy=http://${PROXY_IP}
     export https_proxy=http://${PROXY_IP}
+
+    sudo mkdir -p /etc/systemd/system/containerd.service.d
+    cat <<EOF | sudo tee /etc/systemd/system/containerd.service.d/http-proxy.conf
+[Service]
+Environment="HTTP_PROXY=http://${PROXY_IP}"
+Environment="HTTPS_PROXY=http://${PROXY_IP}"
+# no_proxy 里保留 localhost 和集群内 IP
+Environment="NO_PROXY=127.0.0.1,localhost,10.96.0.0/12,10.244.0.0/16"
+EOF
+    sudo systemctl daemon-reload
+    sudo systemctl restart containerd
+
 }
 
 function unsetProxy() {
     echo "取消代理..."
     unset http_proxy
     unset https_proxy
+    sudo rm /etc/systemd/system/containerd.service.d/http-proxy.conf
+    sudo systemctl daemon-reload
+    sudo systemctl restart containerd
 }
 
 function starrK8s() {
