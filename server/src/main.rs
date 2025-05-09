6666ac95bc81b327c47a847f93b3f9b9b42718c1
WDEP <2633880953@qq.com>
2025-05-10 01:23:33 +0800

    更新todo

--- server/src/main.rs
+++ server/src/main.rs
@@ -6,10 +6,10 @@ mod web;
 #[rocket::main]
 async fn main() {
     // 启动 Redis 操作
-    match db_manager::run_redis() {
-        Ok(_) => println!("Redis operation successful"),
-        Err(e) => println!("Redis operation failed: {}", e),
-    }
+    // match db_manager::run_redis() {
+    //     Ok(_) => println!("Redis operation successful"),
+    //     Err(e) => println!("Redis operation failed: {}", e),
+    // }
 
     // 启动 Rocket 服务器
     if let Err(e) = web::launch_rocket().launch().await {
