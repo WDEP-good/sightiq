6525da2c75cf1c3adaff6ad023a3d3fa19d180e9
WDEP <2633880953@qq.com>
2025-06-21 09:12:58 +0800

    更新脚本

--- scripts/start_redis.sh
+++ scripts/start_redis.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 function startRedis() {
     echo "开始启动Redis..."
-    kubectl apply -f k8s/redis/redis.yaml
+    kubectl apply -f ${SCRIPT_DIR}/k8s/redis/redis.yaml
     echo "Redis启动成功"
 }
 
--- scripts/start_starrocks.sh
+++ scripts/start_starrocks.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 function startStarrocks() {
     echo "开始启动StarRocks..."
     echo "开始部署 StarRocks Cluster CRD"
-    kubectl apply -f k8s/starrocks/starrocks_clusters.yaml
+    kubectl apply -f ${SCRIPT_DIR}/k8s/starrocks/starrocks_clusters.yaml
     echo "开始部署 StarRocks Operator"
-    kubectl apply -f k8s/starrocks/starrocks_operator.yaml
+    kubectl apply -f ${SCRIPT_DIR}/k8s/starrocks/starrocks_operator.yaml
     echo "开始部署 StarRocks FE 和 BE"
-    kubectl apply -f k8s/starrocks/starrocks_fe_and_be.yaml
+    kubectl apply -f ${SCRIPT_DIR}/k8s/starrocks/starrocks_fe_and_be.yaml
     echo "StarRocks启动成功"
 }
 
