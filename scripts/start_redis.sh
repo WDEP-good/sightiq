c688f6ad38101eb945d69e544e4d6b6e0e100118
WDEP <2633880953@qq.com>
2025-06-23 10:55:45 +0800

    添加redis镜像拉取

--- scripts/start_redis.sh
+++ scripts/start_redis.sh
@@ -2,6 +2,7 @@
 
 function startRedis() {
     echo "开始启动Redis..."
+    bash $SIGHTIQ_SCRIPT_DIR/utils/container_proxy_pull.sh -r ${RUNTIME} -p ${PROXY_IP} -k docker.io/redis:latest
     kubectl apply -f $SIGHTIQ_ROOT/k8s/redis/redis.yaml
     echo "Redis启动成功"
 }
